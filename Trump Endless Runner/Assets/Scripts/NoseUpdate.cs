f263eacb7d783553d5ec72c96505098ac5bf32c9
ro-j0 <63885199+ro-j0@users.noreply.github.com>
2021-01-24 17:46:45 -0800

    Trump game added

---
+++ Trump Endless Runner/Assets/Scripts/NoseUpdate.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoseUpdate : MonoBehaviour
+{
+    public int i;
+    // Start is called before the first frame update
+    public GameObject ach;
+
+    bool cou = false;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i = PLayerController.liecount;
+        switch(i){
+            case 1:
+                transform.localScale = new Vector3(1.82f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.26f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 2:
+                transform.localScale = new Vector3(2.24f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.3f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 3:
+                transform.localScale = new Vector3(3.04f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.33f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 4:
+                transform.localScale = new Vector3(4.04f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.38f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 5:
+                transform.localScale = new Vector3(5.84f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.44f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 6:
+                transform.localScale = new Vector3(7.55f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.5f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 7:
+                transform.localScale = new Vector3(11.06f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.62f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            case 8:
+                transform.localScale = new Vector3(16f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.8f,transform.localPosition.y,transform.localPosition.z);
+                ach.SetActive(true);
+                cou = true;
+                break;
+            case 9:
+                transform.localScale = new Vector3(16f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.8f,transform.localPosition.y,transform.localPosition.z);
+                break;
+            default:
+                transform.localScale = new Vector3(0.83f,transform.localScale.y,transform.localScale.z);
+                transform.localPosition = new Vector3(0.26f,transform.localPosition.y,transform.localPosition.z);
+                break;
+        }
+ 
+    }
+
+}
---
+++ Trump Endless Runner/Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool GamePaused;
+    public GameObject pauseMenuUI;
+    public GameObject canvasUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GamePaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame(){
+        //canvasUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void ResumeGame(){
+        //canvasUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
