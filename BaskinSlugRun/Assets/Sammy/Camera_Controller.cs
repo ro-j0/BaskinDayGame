e5247ec8260d7462f4a837dbc08bdb8a0a9ae644
Adrian <adlvasqu@ucsc.com>
2021-02-01 23:56:59 -0800

    Sammy Push

---
+++ BaskinSlugRun/Assets/Sammy/Camera_Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public Transform Player;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = Player.position;
+    }
+}
